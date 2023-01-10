935d02a9538ad275f1726ceefefa71e80b3a48fb
Siparat <maksimak1712@mail.ru>
2023-01-10 13:34:12 +0300

    add command executor

---
+++ src/core/handlers/stream.handler.ts
@@ -0,0 +1,20 @@
+import { ChildProcessWithoutNullStreams } from 'child_process';
+import { IStreamLogger } from './stream-logger.interface'
+
+export class StreamHandler {
+	constructor (private logger: IStreamLogger) {}
+
+	proccessOut(stream: ChildProcessWithoutNullStreams) {
+		stream.stdout.on('data', (data) => {
+			this.logger.log(data)
+		})
+
+		stream.stderr.on('data', (data) => {
+			this.logger.error(data)
+		})
+
+		stream.on('close', () => {
+			this.logger.end()
+		})
+	}
+}
